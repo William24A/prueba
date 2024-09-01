34204655d87adbc69527ab2c0e2909c7de4d4aed
William24A <willi241194@gmail.com>
2024-09-01 17:06:06 -0300

    tercer commit

--- test/TiendaTest.cs
+++ test/TiendaTest.cs
@@ -13,11 +13,43 @@ public class TiendaTest
 
         tiendaPrueba.AgregarProducto(nombre, precio,categoria);
 
-        var producto = tiendaPrueba.BuscarProductos(nombre);
+        var producto = tiendaPrueba.ProductosListados.FirstOrDefault(p => p.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
         Assert.NotNull(producto);
         Assert.Equal(nombre,producto.Nombre);
         Assert.Equal(precio,producto.Precio);
         Assert.Equal(categoria,producto.Categoria);
+    } 
+    [Fact]
+    public void BuscarProductosTest()
+    {
+        // Given
+        Tienda tiendaPrueba = new Tienda();
+        string nombre = "Res";
+        float precio = 1223;
+        string categoria = "carnes";
+        tiendaPrueba.AgregarProducto(nombre, precio,categoria);
+        // When
+        var producto = tiendaPrueba.BuscarProductos(nombre);
+        // Then
+        Assert.NotNull(producto);
+        Assert.Equal(nombre,producto.Nombre);
+        Assert.Equal(precio,producto.Precio);
+        Assert.Equal(categoria,producto.Categoria);
+    }
+
+    [Fact]
+    public void EliminarProductoTest()
+    {
+        Tienda tiendaPrueba = new Tienda();
+        string nombre = "Res";
+        float precio = 1223;
+        string categoria = "carnes";
+
+        tiendaPrueba.AgregarProducto(nombre, precio,categoria);
+
+        var producto = tiendaPrueba.EliminarProducto(nombre);
+
+        Assert.True(producto);
     } 
 
 }
